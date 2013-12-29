17fae7a9a5fb433343c92f5aa9977f5caac8ce23
brettheroux <brett@Bretts-MacBook-Pro.local>
2013-12-28 22:19:06 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SecondRails::Application.config.secret_key_base = '5ce0e9dd7395ac349e3de36ce532d38469475ba3356b87ce9337ef5f01df1d80adfb2fc02f64578cdeef6da4c6eab1930bded7f8c1aa7e617ad3a53bfa966b03'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SecondRails::Application.config.session_store :cookie_store, key: '_second_rails_session'
